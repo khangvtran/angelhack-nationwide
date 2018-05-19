be5a417312bf09bcab42a29dc27f336d5b48ca0c
GeorgeEYokoyama <george_y@outlook.com>
2018-05-19 15:22:31 -0700

    Merge branch 'master' of https://github.com/GeorgeEYokoyama/anglehack-nationwide

--- client/src/services/Nationwide-Api.js
+++ client/src/services/Nationwide-Api.js
@@ -2,11 +2,11 @@ import axios from 'axios'
 
 export default {
   // http://nw-angelhack-2018-mocks.us-east-1.elasticbeanstalk.com/customers
-  get () {
-    return axios.create({ baseURL: 'http://nw-angelhack-2018-mocks.us-east-1.elasticbeanstalk.com/' }).get('customers')
-  }
+  getData (customerId) {
+    return axios.create({ baseURL: 'http://nw-angelhack-2018-mocks.us-east-1.elasticbeanstalk.com/' }).get(customerId)
+  },
 
-  getNationwide(id) {
-  	return axios.create({ baseURL: 'localhost:5000/api/'}).get('customerBalances/' + id)
+  getNationwide (id) {
+    return axios.create({ baseURL: 'localhost:5000/api/' }).get('customerBalances/' + id)
   }
 }
