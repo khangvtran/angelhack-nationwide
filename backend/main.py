9f1a7457938a307c6c46ef3009d0cc2f438b93d0
khangvtran <vinhkhangtran09@gmail.com>
2018-05-19 15:30:20 -0700

    Pythonic Syntax

--- backend/main.py
+++ backend/main.py
@@ -8,27 +8,26 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+
+
 @app.route("/api/customerBalances/<int:id>")
 def getStuff(id):
     income = 0
     customers = requests.get(mock+"/customers")
-    for customer in customers.json():
-        print(customer["id"] == id)
-        if customer["id"] == id:
-            income = customer["householdIncome"]
+    monthly_income = int([customer["householdIncome"] for customer in customers.json() if customer["id"] == id][0]/12)
     balance = getBankBalances(id)
-    dict = {}
-    dict["monthly_salary"] = income
-    dict["savings"] = balance
+    dict = {"monthly_salary":monthly_income, "savings":balance}
     return jsonify(dict), 200
+
+
 
 def getBankBalances(id):
     balance = 0
     accounts = requests.get(mock+"/bankAccounts")
-    for account in accounts.json():
-        if account["customerId"] == id:
-            balance += account["balance"]
+    balance = sum(account["balance"] for account in accounts.json() if account["customerId"] == id)
     return balance
+
+
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', debug=True)
