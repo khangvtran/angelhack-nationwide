72f32fb6e699ff77b58d8b6a50b6ab662a4fbf85
GeorgeEYokoyama <george_y@outlook.com>
2018-05-19 13:53:50 -0700

    Fix indentation errors

--- client/src/components/start_button.vue
+++ client/src/components/start_button.vue
@@ -1,30 +1,30 @@
 <template>
-	<form @submit.prevent="submitHandler">
-		<label for="monthly_salary">Monthly Salary</label>
-		<input type="number" name="monthly_salary" id="monthly_salary" v-model="user.monthly_salary">
-		<label for="savings">Total Savings</label>
-		<input type="number" name="savings" id="savings" v-model="user.savings">
-		<input type="submit" value="Submit">
-	</form>
+  <form @submit.prevent="submitHandler">
+    <label for="monthly_salary">Monthly Salary</label>
+    <input type="number" name="monthly_salary" id="monthly_salary" v-model="user.monthly_salary">
+    <label for="savings">Total Savings</label>
+    <input type="number" name="savings" id="savings" v-model="user.savings">
+    <input type="submit" value="Submit">
+  </form>
 </template>
 
 <script >
-	export default {
-		data() {
-			return {
-				user: {
-					monthly_salary: '',
-					savings: ''
-				}
-			}
-		},
-		methods: {
-			submitHandler(){
-				this.$localStorage.set("monthly_salary", this.user.monthly_salary)
-				this.$localStorage.set("savings", this.user.savings)
-				console.log(this.$localStorage.get("monthly_salary"))
-				this.$router.push({name: 'zillowpage'})
-			}
-		}
-	}
+export default {
+  data () {
+    return {
+      user: {
+        monthly_salary: '',
+        savings: ''
+      }
+    }
+  },
+  methods: {
+    submitHandler () {
+      this.$localStorage.set('monthly_salary', this.user.monthly_salary)
+      this.$localStorage.set('savings', this.user.savings)
+      console.log(this.$localStorage.get('monthly_salary'))
+      this.$router.push({name: 'zillowpage'})
+    }
+  }
+}
 </script>
