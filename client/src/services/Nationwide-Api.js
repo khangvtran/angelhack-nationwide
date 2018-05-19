7a1895c99a1f040cb55b32a03492c5277471e797
Lorman Lau <lormanlau@gmail.com>
2018-05-19 15:37:31 -0700

    build error fix

--- client/src/services/Nationwide-Api.js
+++ client/src/services/Nationwide-Api.js
@@ -4,7 +4,7 @@ export default {
   // http://nw-angelhack-2018-mocks.us-east-1.elasticbeanstalk.com/customers
   get () {
     return axios.create({ baseURL: 'http://nw-angelhack-2018-mocks.us-east-1.elasticbeanstalk.com/' }).get('customers')
-  }
+  },
 
   getNationwide(id) {
   	return axios.create({ baseURL: 'localhost:5000/api/'}).get('customerBalances/' + id)
