463255afa20edec037d18ae9f2d13354ec42461b
Lorman Lau <lormanlau@gmail.com>
2018-05-19 22:44:40 -0700

    google maps with marker

--- client/src/main.js
+++ client/src/main.js
@@ -6,10 +6,20 @@ import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import VueLocalStorage from 'vue-localstorage'
+import * as VueGoogleMaps from "vue2-google-maps";
+import key from './../config/config.js'
 
 Vue.config.productionTip = false
 Vue.use(Vuetify)
 Vue.use(VueLocalStorage)
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: key,
+    libraries: "places" // necessary for places input
+  }
+});
+
 
 /* eslint-disable no-new */
 new Vue({
