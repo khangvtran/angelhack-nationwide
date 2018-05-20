16d0bfe3c8a4f20f0fe9436b7db1c638f948480a
GeorgeEYokoyama <george_y@outlook.com>
2018-05-19 17:15:41 -0700

    additionalinfo.vue takes input values and store them in localStorage

--- client/src/components/additionalinfo.vue
+++ client/src/components/additionalinfo.vue
@@ -1,11 +1,16 @@
 <template>
-  <form @submit.prevent="submitHandler">
-    <label for="percent">Percent of month income you want to set aside for a home</label>
-    <input type="number" name="percent" id="percent" v-model="user.percent">
-    <label for="waitmonths">How long do you want to wait until you buy a home?</label>
-    <input type="number" name="waitmonths" id="waitmonths" v-model="user.waitmonths">
-    <input type="submit" name="submit" value="submit">
-  </form>
+  <div class="additionalinfo">
+    <h1>Additional Information</h1>
+    <form @submit="submitHandler">
+      <label for="percent">Percent of month income you want to set aside for a home</label>
+      <input type="number" name="percent" id="percent" v-model="user.percent">
+      <br>
+      <label for="waitmonths">How long do you want to wait until you buy a home?</label>
+      <input type="number" name="waitmonths" id="waitmonths" v-model="user.waitmonths">
+      <br>
+      <button type="submit" name="submit">Submit</button>
+    </form>
+  </div>
 </template>
 
 <script scoped>
@@ -15,16 +20,15 @@ export default {
       user: {
         monthly_salary: JSON.parse(this.$localStorage.get('user')).monthly_salary,
         savings: JSON.parse(this.$localStorage.get('user')).savings,
-        percent: '',
-        waitmonths: ''
+        percent: null,
+        waitmonths: null
       }
     }
   },
   methods: {
-    submitHandler () {
+    submitHandler (e) {
+      e.preventDefault()
       this.$localStorage.set('user', JSON.stringify(this.user))
-      let object = this.$localStorage.get('user')
-      console.log(object)
     }
   }
 }
