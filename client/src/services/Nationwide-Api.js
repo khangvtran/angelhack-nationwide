a38403d68af0256b7fad91e7996ef20da7890850
GeorgeEYokoyama <george_y@outlook.com>
2018-05-19 17:19:49 -0700

    query string formatted for backend api endpoint

--- client/src/services/Nationwide-Api.js
+++ client/src/services/Nationwide-Api.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export default {
   getNationwide (id) {
-    return axios.create({ baseURL: 'http://localhost:5000/api/' }).get('customerBalances/' + id)
+    return axios.create({ baseURL: 'http://localhost:5000/api/' }).get('customerBalances/?id=' + id)
   }
 }
