2dcebc1de25a429f5f73840639108cb4586bd38c
Lorman Lau <lormanlau@gmail.com>
2018-05-19 19:34:41 -0700

    api call should wokr

--- client/src/services/Nationwide-Api.js
+++ client/src/services/Nationwide-Api.js
@@ -1,11 +1,17 @@
 import axios from 'axios'
 
+let headers = {
+  'Access-Control-Allow-Origin' : '*',
+  'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+  'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token'
+}
+
 export default {
   getNationwide (id) {
     return axios.create({ baseURL: 'http://localhost:5000/api/' }).get('customerBalances/' + id)
   },
   processData (user) {
-    return axios.create({baseURL: 'http://localhost:5000/api/'}).post('processTotalFunds', user)
+    return axios.create({baseURL: 'http://localhost:5000/api/'}).post('processTotalFunds', user, headers)
   },
   callOnboardApi (data) {
     // return axios.create({baseURL: ""}).post('', data)
