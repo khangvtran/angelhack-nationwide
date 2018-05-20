0bff56e912081ff3d58dae160fb05e54eecc72af
khangvtran <vinhkhangtran09@gmail.com>
2018-05-19 17:02:55 -0700

    processTotalFunds

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 import requests
 from flask import Flask, request, jsonify, render_template
-
+import ast
 mock = "http://nw-angelhack-2018-mocks.us-east-1.elasticbeanstalk.com/"
 app = Flask(__name__,
     static_folder = "./../client/dist/static",
@@ -10,7 +10,7 @@ app = Flask(__name__,
 def hello():
     return render_template("index.html")
 
-
+# get data from GET
 @app.route("/api/customerBalances/<int:id>")
 def getStuff(id):
     customers = requests.get(mock+"/customers") #<class 'requests.models.Response'>, use .json() to convert to list
@@ -25,21 +25,45 @@ def getBankBalances(id):
     balance = sum(account["balance"] for account in accounts.json() if account["customerId"] == id)
     return balance
 
+
+# get data from POST 
 @app.route("/api/processTotalFunds", methods = ['POST'])
 def getTotalFunds():
-	data = request.data.json()
-	print(type(data))
-	print(data)	
-	return jsonify(data), 200
+	data = request.data #<string>
+	data = ast.literal_eval(data)  #<dictionary>
+	monthlyIncome = float(data["monthly_salary"])
+	saving = float(data["saving"])
+	percent = int(data["percent"])
+	waitmonths = int(data["wait_months"])
+	int mortgageYears = int(data["mortgage_years"])
+
+	monthlyPayment = monthlyIncome*percent/100
+	downPayment = monthlyPayment*waitmonths + saving
+	totalMortgagePayment = 12*mortgageYears*monthlyPayment
+
+	totalPayment = downPayment + totalMortgagePayment
+
+	return jsonify(totalPayment), 200
+
+
 
 
 
 '''
+
+>>> import ast
+>>> ast.literal_eval("{'muffin' : 'lolz', 'foo' : 'kitty'}")
+{'muffin': 'lolz', 'foo': 'kitty'}
+
+
+
 {
 	"monthly_salary": "200000",
 	"savings": "1000000",
 	"percent": "30",
-	"waitmonths": "12"
+	"wait_months": "12"
+	"mortgage_years": "40"
+	""
 }
 '''
 
